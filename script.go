f2b25fda62c13b93c6b4f1acf792f75b8dddfaac
root <root@ff2.eaw.com.pl>
2018-05-16 01:19:02 +0200

    - added CMTS MIC calculation - fixed MTA config generation (TLV254)

--- script.go
+++ script.go
@@ -3,8 +3,10 @@ package main
 import (
     "net"
     "fmt"
+    "io"
     "bytes"
     "net/url"
+    "crypto/hmac"
     "crypto/md5"
     "encoding/hex"
     "encoding/binary"
@@ -14,6 +16,7 @@ import (
     "github.com/mattn/anko/core"
     "github.com/mattn/anko/parser"
     "github.com/mattn/anko/packages"
+    "github.com/spf13/viper"
 )
 
 type Script struct {
@@ -21,7 +24,7 @@ type Script struct {
     env *vm.Env
     db *sqlx.DB
     query url.Values
-    tlvOutput map[uint8][]byte
+    Output *bytes.Buffer
 }
 
 func NewScript(filename string, db *sqlx.DB, query url.Values) *Script {
@@ -76,7 +79,7 @@ func NewScript(filename string, db *sqlx.DB, query url.Values) *Script {
     core.Import(env)
     packages.DefineImport(env)
 
-    output := make(map[uint8][]byte)
+    output := bytes.NewBuffer(nil)
     return &Script{string(b), env, db, query, output}
 }
 
@@ -85,7 +88,7 @@ func (e *Script) Execute() (*bytes.Buffer, error) {
     e.env.Define("sql_row", e.SqlQueryRow)
     e.env.Define("query", e.query)
 
-    out, err := e.env.Execute(e.filename)
+    _, err := e.env.Execute(e.filename)
     if err != nil {
 	switch e := err.(type) {
 	    case *parser.Error:
@@ -97,32 +100,16 @@ func (e *Script) Execute() (*bytes.Buffer, error) {
 	}
     }
 
-    res := bytes.NewBuffer(nil)
-    switch t, _ := e.env.Get("config_type"); t {
-	case "cm", "mta":
-	    var keys []int
-	    for k := range e.tlvOutput {
-		if k != 6 && k != 7 {
-		    keys = append(keys, int(k))
-		}
-	    }
-	    for _, k := range keys {
-	        res.Write(e.tlvOutput[uint8(k)])
-	    }
-	    if t == "cm" {
-		e.CmMic(res.Bytes())
-		res.Write(e.tlvOutput[6])
-		res.Write(e.tlvOutput[7])
-	    }
-	default:
-	    res.Write(out.([]byte))
+    if t, _ := e.env.Get("config_type"); t == "cm" {
+	e.CmMic()
+	e.CmtsMic()
     }
 
-    return res, nil
+    return e.Output, nil
 }
 
 func (e *Script) TlvAdd( t int64, v ...[]byte ) {
-    e.tlvOutput[uint8(t)] = append(e.tlvOutput[uint8(t)], TLV(t, v...)... )
+    e.Output.Write(TLV(t,v...))
 }
 
 func (e *Script) SqlQueryRow(query string, args ...interface{}) map[string]interface{} {
@@ -135,26 +122,43 @@ func (e *Script) SqlQueryRow(query string, args ...interface{}) map[string]inter
     return object
 }
 
-func (e *Script) CmMic(data []byte) {
+func (e *Script) CmMic() {
     digest := md5.New()
-    digest.Write(data)
-    e.tlvOutput[6] = TLV(6, digest.Sum(nil) )
+    digest.Write(e.Output.Bytes())
+    e.TlvAdd( 6, digest.Sum(nil) )
 }
 
-func (e *Script) CmtsMic(data []byte) {
-    /*
+func (e *Script) CmtsMic() {
+    mac := hmac.New(md5.New, []byte(viper.GetString("shared_secret")))
     fields := []uint8{ 1, 2, 3, 4, 17, 43, 6, 18, 19, 20, 22, 23, 24, 25, 28, 29, 26, 35, 36, 37, 40 }
 
-    data = self.data + [ cm_mic ]
-    for i in fields:
-	for j in data:
-	    if i == j.t:
-	        binstring += str(j)
-	mic = hmac.new(self.secret, binstring).digest()
+    for _, f := range fields {
+	tlvs := bytes.NewBuffer(e.Output.Bytes())
+	for {
+	    var t, l uint8
+	    err := binary.Read(tlvs, binary.BigEndian, &t)
+	    if err != nil {
+		break
+	    }
+	    err = binary.Read(tlvs, binary.BigEndian, &l)
+	    if err != nil {
+		break
+	    }
+	    v := make([]byte, l)
+	    _, err = io.ReadFull(tlvs, v)
+	    if err != nil {
+		break
+	    }
+	    if t == uint8(f) {
+		mac.Write([]byte{t, l})
+		mac.Write(v)
+	    }
+	}
+    }
 
-    e.tlvOutput[7] = TLV(7, md5()) 
-    */
+    e.TlvAdd( 7, mac.Sum(nil) )
 }
+
 
 /*
 func (e *Script) Pad() {
